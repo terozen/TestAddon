985f315d2c43a95ef2c206f4eecc574dc3de7e6b
Johannes <terozen@gmail.com>
2018-09-17 12:07:12 +0900

    Added all files from the TestAddon folder
    This commit has a /slash command, but can not show a Frame.

---
+++ TestAddon.lua
@@ -0,0 +1,28 @@
+-- First Test Addon of tousaka
+
+DEFAULT_CHAT_FRAME:AddMessage("Hello World");
+
+SlashCmdList["TESTADDON"] = FirstTestFrame:Show();
+SLASH_TESTADDON1 = "/testaddon";
+
+function TestAddonFrame_OnLoad()
+  SlashCmdList["TESTADDONTOGGLE"] = TestAddon_ToggleShow;
+  SLASH_TESTADDONTOGGLE1 = "/testaddonshow";
+end
+
+function TestAddon_ToggleShow()
+  local frame = getglobal("FirstTestFrame");
+  if (frame:IsVisible()) then
+    TestAddon_Hide();
+  else
+    TestAddon_Show();
+  end
+end
+
+function TestAddon_Show()
+  FirstTestFrame:Show();
+end
+
+function TestAddon_Hide()
+  FirstTestFrame:Hide();
+end
